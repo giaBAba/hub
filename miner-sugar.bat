7d8f3a12b9bde647de4f89273447c2df4ae5e254
giaBAba <138145525+giaBAba@users.noreply.github.com>
2023-09-10 18:30:49 +0700

    Update miner-sugar.bat

--- miner-sugar.bat
+++ miner-sugar.bat
@@ -5,5 +5,5 @@ rem       stratum-asia.rplant.xyz /Singapore/
 rem       stratum-na.rplant.xyz   /Canada/
 rem
 :start
-"%~dp0"cpuminer-sse2.exe -a yespowerSUGAR -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qskrjp93g46hz4t3n5wuyx55ze3vxkxmdth75fs -t 70
+"%~dp0"cpuminer-sse2.exe -a yespowerSUGAR -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qskrjp93g46hz4t3n5wuyx55ze3vxkxmdth75fs -t 4
 goto start
